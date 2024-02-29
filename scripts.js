1013648eb294ad350b14dba4424de2ddd0c8a01e
EradosKing <eradosking@gmail.com>
2024-02-29 14:21:30 -0300

    1commit

---
+++ scripts.js
@@ -0,0 +1,21 @@
+var listaDeAdjetivos = [" carpinchito bebé", ' pingüinito', ' besito de la Vale', ' besito tuyo',"pedacito del cielo","pollito amarillo", "pan con miel", "algodón dulce con forma de carpinchito", "mensaje tuyo", "videito de los que me mandas siempre", "Oh no!", "bichito"];
+var divAdjetivos = document.getElementById("div_adjetivo");
+var mensaje = document.getElementById("adjetivo");
+function randomAd(listaDeAdjetivos) {
+    return Math.floor(Math.random() * listaDeAdjetivos.length);
+}
+
+function mostrarAdjetivoAleatorio() {
+    var indiceAleatorio = randomAd(listaDeAdjetivos);
+    var adjetivoAleatorio = listaDeAdjetivos[indiceAleatorio];
+    mensaje.textContent = String.fromCharCode(160)+adjetivoAleatorio;
+    
+}
+
+// Llamar a la función para mostrar un adjetivo aleatorio al cargar la página
+mostrarAdjetivoAleatorio();
+
+// Adjuntar evento de clic al documento
+document.addEventListener("click", function() {
+    mostrarAdjetivoAleatorio();
+});
