b928d88c5d5706941fbaf6642a7b5cae35fd9b0c
EradosKing <eradosking@gmail.com>
2024-03-05 14:48:02 -0300

    Test

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
-var listaDeAdjetivos = [" carpinchito bebé", ' pingüinito', ' besito de la Vale', ' besito tuyo',"pedacito del cielo","pollito amarillo", "pan con miel", "algodón dulce con forma de carpinchito", "mensaje tuyo", "videito de los que me mandas siempre", "Oh no!", "bichito","manso pa las balas", '"Mi amor" que me djiste sin querer'];
+//var listaDeAdjetivos = [" carpinchito bebé", ' pingüinito', ' besito de la Vale', ' besito tuyo',"pedacito del cielo","pollito amarillo", "pan con miel", "algodón dulce con forma de carpinchito", "mensaje tuyo", "videito de los que me mandas siempre", "Oh no!", "bichito","manso pa las balas", '"Mi amor" que me djiste sin querer',];
 var divAdjetivos = document.getElementById("div_adjetivo");
+var listaDeAdjetivos= [" No se si volviste a abrir esto alguna otra vez"]
 var mensaje = document.getElementById("adjetivo");
 function randomAd(listaDeAdjetivos) {
     return Math.floor(Math.random() * listaDeAdjetivos.length);
